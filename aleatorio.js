9e338d23ae6498c0a225e565bfd9c3792200c33b
Gabriel-Eduardokluska <kluska.gabriel@escola.pr.gov.br>
2026-09-20 18:54:04 -0300

    Update aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,8 +1,32 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomesAventureiros = [
+    "Link",
+    "Sheikah",
+    "Explorador",
+    "Herói de Hyrule",
+    "Aventureiro",
+    "Guardião de Hyrule"
+];
 
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
+export function aleatorio(lista) {
+    if (!lista || lista.length === 0) {
+        return "";
+    }
+
+    const posicao = Math.floor(Math.random() * lista.length);
+
     return lista[posicao];
 }
 
-export const nome = aleatorio(nomes)
+export function embaralhar(lista) {
+    const copia = [...lista];
+
+    for (let i = copia.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [copia[i], copia[j]] = [copia[j], copia[i]];
+    }
+
+    return copia;
+}
+
+export const nomeAventureiro = aleatorio(nomesAventureiros);
